93a8191fd31e951429724c57f043adb163673fbb
Rafael Monteiro Rodrigues <rafaelmontro1996@gmail.com>
2023-07-11 14:32:31 -0300

    feat: fetch starship data from SWAPI

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -1,8 +1,40 @@
-//import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import './App.css'
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://swapi.dev/api/starships/');
+        if (response.ok) {
+          const jsonData = await response.json();
+          const ships = [];
+          let i = 0;
+          for(const ship of jsonData.results) {
+            const newRecord = {'id': i,
+                               'name': ship.name,
+                               'MGLT': Number(ship.MGLT),
+                               'consumables': ship.consumables
+              }
+            ships.push(newRecord);
+            i++;
+          }
+          setData(ships);
+        }
+        else {
+          console.error('GET request failed with status code:', response.status);
+        }
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    };
+
+    fetchData();
+
+  }, []);
 
   return (
     <>
